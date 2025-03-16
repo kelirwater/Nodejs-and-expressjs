f5bba76ce494048ff75635f23a700d8a12003479
naveen <kelirwater99@gmail.com>
2025-03-16 15:06:46 +0530

    reading,Creating,directories,deleteof files system

---
+++ files.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+
+//reading files
+//asynchronous file
+// fs.readFile("./docs/blog12.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(data.toString());
+// });
+
+// console.log("last line");
+
+//print lastline first
+//after prints reading file
+
+//writing files
+// fs.writeFile("./docs/blog1.txt", "hello, world", () => {
+//   console.log("file written");
+// });
+
+// fs.writeFile("./docs/blog2.txt", "hello, again", () => {
+//   console.log("file written");
+// });
+
+//directories
+// if (!fs.existsSync("./assets")) {
+//   fs.mkdir("./assets", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder created");
+//   });
+// } else {
+//   fs.rmdir("./assets", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder deleted");
+//   });
+// }
+
+//delete files
+fs.unlink("./docs/deleteme.txt", (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("files deleted");
+});
