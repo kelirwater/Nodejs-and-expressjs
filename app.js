c92734975b9b815618b1a0ba26cdbfc199e49bbe
naveen <kelirwater99@gmail.com>
2025-03-17 18:12:56 +0530

    view engines with new way injecting html pages to server with ejs

--- app.js
+++ app.js
@@ -4,10 +4,38 @@ const express = require("express");
 
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 //listen for request
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 //console.log("listening on port 3000");
 
 app.get("/", (req, res) => {
-  res.send("<p> home page </p>");
+  // res.send("<p> home page </p>");
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  // res.send("<p> about page </p>");
+  //res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
+});
+
+//redirects
+
+// app.get("/about-us", (req, res) => {
+//   res.redirect("/about");
+// });
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
+});
+
+//404 page it should be always last.
+app.use((req, res) => {
+  res.status(404).render("404"); //sendFile("./views/404.html", { root: __dirname });
 });
